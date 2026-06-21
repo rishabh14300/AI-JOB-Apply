648661cc50456d529fdb4561a33b168760b5acd1
Rishabh Bhadani <rishabh.bhadani@digiwhiz.com.au>
2026-06-21 22:29:32 +0530

    Fixed Cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,10 +13,15 @@ dotenv.config();
 const app = express();
 const PORT = Number(process.env.PORT) || 5000;
 
-app.use(cors({
-  origin: process.env.CLIENT_URL || '*',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      process.env.CLIENT_URL!
+    ],
+    credentials: true,
+  })
+);  
 app.use(express.json({ limit: '50mb' }));
 
 // Routes
